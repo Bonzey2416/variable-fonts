b6fbc826dc724eaa20592f6c99adbd48e0ddd646
Bonzey2416 <98016194+Bonzey2416@users.noreply.github.com>
2024-02-02 12:15:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var slider = document.getElementById("fontSize");
 var output = document.getElementById("fontSizePreview");
+var preview1 = document.getElementById("textPreview1");
+var preview2 = document.getElementById("textPreview2");
+var preview3 = document.getElementById("textPreview3");
+
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
   output.innerHTML = this.value;
+  preview1.style.fontSize = this.value + "px";
+  preview2.style.fontSize = this.value + "px";
+  preview3.style.fontSize = this.value + "px";
 };
