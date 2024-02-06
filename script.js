4891b23e2be3ddb3c863349b202b375a776e869b
Bonzey2416 <98016194+Bonzey2416@users.noreply.github.com>
2024-02-06 14:46:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 var slider = document.getElementById("fontSize");
 var output = document.getElementById("fontSizePreview");
 var root = document.querySelector(":root");
-var elementw = "element" + "w";
 var gsf = document.getElementById("GoogleSansFlex");
 var gsfw = document.getElementById("GoogleSansFlexWeight");
 var gsfwslider = document.getElementById("GoogleSansFlexWeightSlider");
+var gsfwi = document.getElementById("GoogleSansFlexWidth");
+var gsfwislider = document.getElementById("GoogleSansFlexWidthSlider");
 var rf = document.getElementById("RobotoFlex");
 var rfw = document.getElementById("RobotoFlexWeight");
 var rfwslider = document.getElementById("RobotoFlexWeightSlider");
+var rfwi = document.getElementById("RobotoFlexWidth");
+var rfwislider = document.getElementById("RobotoFlexWidthSlider");
 var sfpv = document.getElementById("SFProVariable");
 var sfpvw = document.getElementById("SFProVariableWeight");
 var sfpvwslider = document.getElementById("SFProVariableWeightSlider");
+var sfpvwi = document.getElementById("SFProVariableWidth");
+var sfpvwislider = document.getElementById("SFProVariableWidthSlider");
 
 output.innerHTML = slider.value;
 gsfw.innerHTML = gsfwslider.value;
+gsfwi.innerHTML = gsfwislider.value;
+rfw.innerHTML = rfwslider.value;
+rfwi.innerHTML = rfwislider.value;
+sfpvw.innerHTML = sfpvwslider.value;
+sfpvwi.innerHTML = sfpvwislider.value;
 
 slider.oninput = function () {
   output.innerHTML = this.value;
@@ -25,9 +35,25 @@ function fontWeightChange(element, fontweight) {
   elementw.innerHTML = this.value;
 }
 
+function fontWidthChange(element, fontwidth) {
+  element.style.fontStretch = fontwidth + "%";
+  elementwi.innerHTML = this.value;
+}
+
+function fontSizeChange(number) {
+  output.innerHTML = number;
+  slider.value = number;
+  root.style.setProperty("--font-preview-size", number + "px");
+}
+
 gsfwslider.oninput = function () {
   gsfw.innerHTML = this.value;
   gsf.style.fontWeight = this.value;
+};
+
+gsfwislider.oninput = function () {
+  gsfwi.innerHTML = this.value;
+  gsf.style.fontStretch = this.value + "%";
 };
 
 rfwslider.oninput = function () {
@@ -35,7 +61,17 @@ rfwslider.oninput = function () {
   rf.style.fontWeight = this.value;
 };
 
+rfwislider.oninput = function () {
+  rfwi.innerHTML = this.value;
+  rf.style.fontStretch = this.value + "%";
+};
+
 sfpvwslider.oninput = function () {
   sfpvw.innerHTML = this.value;
   sfpv.style.fontWeight = this.value;
+};
+
+sfpvwislider.oninput = function () {
+  sfpvwi.innerHTML = this.value;
+  sfpv.style.fontStretch = this.value + "%";
 };
